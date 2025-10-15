a4dce4ec9373fd842c7bcd3aa76edd0feec38c95
gust-eng <gustavolana232@gmail.com>
2025-10-15 16:17:59 -0300

    subir

---
+++ frontend/src/components/paginaPrincipal/2parte/index.jsx
@@ -0,0 +1,35 @@
+import "./parte2.scss";
+
+export default function Parte2() {
+  return (
+    <section className="parte2">
+      <div className="onda-topo">
+        <svg viewBox="0 0 1440 320" preserveAspectRatio="none">
+              <path fill="#f6e9e8" d="M0,64 C360,200 1000,0 1440,96 L1440,0 L0,0 Z" />
+        </svg>
+      </div>
+
+      <div className="conteudo">
+        <h2>Para quem é o Conectando Gerações?</h2>
+
+        <div className="text">
+          <img
+            className="imga"
+            src="/src/assets/images/download (4).jpeg"
+            alt="Idosos aprendendo tecnologia"
+          />
+
+          <div className="texto">
+            <h3>60+</h3>
+            <p>
+              O Conectando Gerações é um site criado para ajudar idosos a usar a
+              internet com mais segurança e confiança. Nossa missão é orientar,
+              ensinar e proteger, oferecendo dicas práticas para navegar online,
+              evitar golpes e se adaptar às facilidades do mundo digital.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,12 +1,18 @@
 import "./index.scss";
 import Cabecalho from "../components/cabecalho/cabecalho";
 import Parte1 from "../components/paginaPrincipal/1parte";
+import Parte2 from "../components/paginaPrincipal/2parte";
+
 
 export default function Inicio () {
     return(
         <div className="container-inicio">
             <Cabecalho/>
           <Parte1/>
+          <div className="separacao">
+
+          </div>
+          <Parte2/>
         </div>
     );
 }
